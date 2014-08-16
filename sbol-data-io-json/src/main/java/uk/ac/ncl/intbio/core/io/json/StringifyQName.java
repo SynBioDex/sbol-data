738df107ce805e916c6e866961e08f832d455fc2
goksel <goksel.misirli@newcastle.ac.uk>
2014-08-16 10:26:22 +0100

    Added curly brackets for QName serialisation

--- sbol-data-io-json/src/main/java/uk/ac/ncl/intbio/core/io/json/StringifyQName.java
+++ sbol-data-io-json/src/main/java/uk/ac/ncl/intbio/core/io/json/StringifyQName.java
@@ -12,16 +12,16 @@ public class StringifyQName {
   public static NameTransformer<QName, String> qname2string = new NameTransformer<QName, String>() {
     @Override
     public String transformName(QName qName) {
-      return qName.getNamespaceURI() + qName.getLocalPart();
+      return "{" + qName.getNamespaceURI() + "}" + qName.getLocalPart();
     }
   };
 
   public static NameTransformer<String, QName> string2qname = new NameTransformer<String, QName>() {
     @Override
     public QName transformName(String f) {
-      int hash = f.indexOf("#") + 1;
-      String prefix = f.substring(0, hash);
-      String suffix = f.substring(hash, f.length());
+      int hash = f.indexOf("}");
+      String prefix = f.substring(1, hash);
+      String suffix = f.substring(hash+1, f.length());
       return Datatree.QName(prefix, suffix);
     }
   };
