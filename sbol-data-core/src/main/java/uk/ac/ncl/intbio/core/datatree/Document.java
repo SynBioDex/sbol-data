fbd33bcd9fed5a7ba8da0fb03779dd3a59948b67
Matthew Pocock <turingatemyhamster@gmail.com>
2014-08-21 20:17:00 +0100

    Minor cleanup for Document.

--- sbol-data-core/src/main/java/uk/ac/ncl/intbio/core/datatree/Document.java
+++ sbol-data-core/src/main/java/uk/ac/ncl/intbio/core/datatree/Document.java
@@ -1,23 +1,22 @@
 package uk.ac.ncl.intbio.core.datatree;
 
-import javax.xml.namespace.QName;
 import java.util.List;
 
 public interface Document<N, P extends PropertyValue> {
 	public List<NamedProperty<N, P>> getProperties();
 	public List<NamespaceBinding> getNamespaceBindings();
 
-    P getProperty(N rdfType);
+  P getProperty(N rdfType);
 
-    public static abstract class Abstract<N, P extends PropertyValue>  implements Document<N, P> {
-        @Override
-        public P getProperty(N propety) {
-            for(NamedProperty<N, P> p : getProperties()) {
-                if(p.getName().equals(propety)) {
-                    return p.getValue();
-                }
-            }
-            return null;
+  public static abstract class Abstract<N, P extends PropertyValue>  implements Document<N, P> {
+    @Override
+    public P getProperty(N property) {
+      for(NamedProperty<N, P> p : getProperties()) {
+        if(p.getName().equals(property)) {
+          return p.getValue();
         }
+      }
+      return null;
     }
+  }
 }
